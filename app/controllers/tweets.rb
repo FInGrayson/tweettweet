1bc5e1221aa7f425e2369b125b83c7b6f4c64ccf
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-15 15:23:54 -0500

    Add tweet routes, new form, and partials.

--- app/controllers/tweets.rb
+++ app/controllers/tweets.rb
@@ -1,9 +1,27 @@
 get '/tweets' do
   @tweets = Tweet.all
-  erb :'/index'
+  erb :'tweets/index'
 end
 
+
+get '/tweets/new' do
+  require_user
+  current_user
+  erb :'tweets/new'
+end
+
+
 post '/tweets' do
-  @tweet = Tweet.find(params[:id])
-  erb :'/index', 
+  current_user
+  @tweet = Tweet.new(params[:info])
+  if request.xhr?
+    if @tweet.save
+      status 200
+      erb :'_partial/_tweet', layout: false, locals: (tweet: @tweet)
+    else
+      status 422
+    end
+  else
+    redirect '/'
+  end
 end
