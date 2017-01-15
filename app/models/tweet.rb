1e24a218dab7efb854c1235df7c319f09351aa2c
Jefferson-Faseler <jjfaseler@gmail.com>
2017-01-15 15:03:14 -0500

    Add validation to tweet class.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :users
+
+  validates: :tweet, presence: true, limit: {in: 1..150,
+    too_long: "%{count} characters max", too_short: 'Your tweet cannot be empty' }
 end
